accf4cf1f89fa0d2531388ba94e93583e6c7c2ca
Teacher <teacher.informatics26@gmail.com>
2024-11-20 18:46:22 +0200

    Change file

--- src/menu.js
+++ src/menu.js
@@ -1,24 +1,9 @@
 
 
 let toggle = document.querySelector('.toggle');
-let close = document.querySelector('.close');
-let burger_menu = document.querySelector('nav');
-let header = document.querySelector('header');
-toggle.addEventListener('click', showMenu);
-close.addEventListener('click', hideMenu);
-function showMenu(){
-  burger_menu.style.display='block';
-  close.style.display='block';
-  toggle.style.display='none';
-  header.style.borderBottomColor='white';
-  header.style.borderBottomStyle='solid';
+let my_nav = document.querySelector('nav');
 
 
-}
-
-function hideMenu(){
-  burger_menu.style.display='none';
-  toggle.style.display='block';
-  close.style.display='none';
-  header.style.border='none';
-}
+toggle.addEventListener('click', () => {
+  my_nav.classList.toggle('show');
+});
